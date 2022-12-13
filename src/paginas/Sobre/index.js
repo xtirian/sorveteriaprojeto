7a8f25ed27bf6619f35bdba762b658dc84d15afc
Matheus Fernandes <108370815+xtirian@users.noreply.github.com>
2022-12-13 19:18:27 +0000

    adicionando o responsivo na página sovre e sabores

--- src/paginas/Sobre/index.js
+++ src/paginas/Sobre/index.js
@@ -1,11 +1,31 @@
 import React from "react";
 import Cabecalho from "../../components/Topo";
+import Footer from "../../components/Footer"
+import "./style.css"
 
 function PaginaSobre () {
     return(
         <div>
             <Cabecalho />
-            <p>Página Sobre</p>
+            <main>
+                <section className="banner-sobre">
+                    <h1>A GELATERIA</h1>
+                </section>
+                <section className="sobre-text">
+                    <div className="limitar-secao">
+                        <h2>Sobre Nós</h2>
+                        <h3>Nós simplesmente amamos sorvete</h3>
+
+                        <p>Somos ums empresa apaixonada pelo que faz. Colocamos amor em cada sorvete produzido. Fazemos o melhor sorvete para os nossos clientes e gostamos de receber elogios. Estamos operando desde 2009 com as melhores matérias-primas para a produção final do sorvete. Vendemos tanto para varejo como para atacado.</p>
+                        <p>Nossos clientes podem comprar os nossos sorvetes e degustar na nossa loja, ou levar para sua residência e aproveitar junto com a família. Também vendemos para estabelecimentos e criamos eventos como festa de aniversário, formaturas e eventos empresariais. Para contratar os nossos serviços, basta entrar em contato conosco. Iremos poporcionar o melhor atendimento e os melhores produtos para você fazer a sua festa mais saborosa, com o melhor sorvete da cidade.</p>
+                    </div>
+                </section>
+                <section className="sobre-showindows">
+                    <img src="/assets/sobre-image.jpg" alt="sobre-imagem" />
+                    <img src="/assets/sorveteria.jpg" alt="loja-imagem" />
+                </section>
+            </main>
+            <Footer />
         </div>
     )
 }
